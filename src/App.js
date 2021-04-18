63707e806160a8cc549d3e03ba8f4153a48729a8
Serhii Karavashkin <ksawl@narod.ru>
2021-04-18 20:32:12 +0300

    remove unuse elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import NavbarTop from './components/navbar/NavbarTop';
+import Clients from './components/clients/Clients';
 
 function App() {
     return (
         <div className="App">
             <NavbarTop />
+
+            <main>
+                <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                    <Clients />
+                </div>
+            </main>
         </div>
     );
 }
