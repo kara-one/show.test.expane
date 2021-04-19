026d5e61e5a528cde28354570feb1010c3d4deeb
Serhii Karavashkin <ksawl@narod.ru>
2021-04-19 22:32:56 +0300

    fix UI Buttons style

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -9,14 +9,14 @@ import {
 } from '@heroicons/react/solid';
 
 const colors = {
-    light: `border-gray-200 text-gray-400 bg-white hover:bg-gray-200 hover:text-black active:bg-gray-50 active:text-black focus:ring-gray-500`,
-    'light-danger': `border-gray-200 text-gray-400 bg-white hover:bg-red-500 hover:text-white active:bg-red-500 active:text-white focus:ring-red-500`,
-    primary: `border-blue-300 text-blue-500 bg-blue-100 hover:bg-blue-700 hover:text-white active:bg-blue-700 active:text-white focus:ring-indigo-500`,
-    success: `border-green-300 text-green-700 bg-green-100 hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white focus:ring-green-500`,
-    danger: `border-red-300 text-red-700 bg-red-100 hover:bg-red-500 hover:text-white active:bg-red-500 active:text-white focus:ring-red-500`,
-    dark: `border-black text-gray-900 bg-gray-100 hover:bg-black hover:text-white active:bg-black active:text-white focus:ring-black`,
-    warning: `border-yellow-300 text-yellow-700 bg-yellow-100 hover:bg-yellow-500 hover:text-white active:bg-yellow-500 active:text-white focus:ring-yellow-500`,
-    indigo: `border-indigo-300 text-indigo-700 bg-indigo-100 hover:bg-indigo-900 hover:text-white active:bg-indigo-900 active:text-white focus:ring-indigo-500`,
+    light: `border-gray-200 text-gray-400 bg-white hover:bg-gray-200 hover:text-black active:bg-gray-50 active:text-black focus:ring-gray-200 focus:bg-gray-200 focus:text-black`,
+    'light-danger': `border-gray-200 text-gray-400 bg-white hover:bg-red-500 hover:text-white active:bg-red-500 active:text-white focus:ring-red-500 focus:bg-red-500 focus:text-white`,
+    primary: `border-blue-300 text-blue-500 bg-blue-100 hover:bg-blue-700 hover:text-white active:bg-blue-700 active:text-white focus:ring-indigo-500 focus:bg-blue-700 focus:text-white`,
+    success: `border-green-300 text-green-700 bg-green-100 hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white focus:ring-green-500 focus:bg-green-500 focus:text-white`,
+    danger: `border-red-300 text-red-700 bg-red-100 hover:bg-red-500 hover:text-white active:bg-red-500 active:text-white focus:ring-red-500 focus:bg-red-500 focus:text-white`,
+    dark: `border-black text-gray-900 bg-gray-100 hover:bg-black hover:text-white active:bg-black active:text-white focus:ring-black focus:bg-black focus:text-white`,
+    warning: `border-yellow-300 text-yellow-700 bg-yellow-100 hover:bg-yellow-500 hover:text-white active:bg-yellow-500 active:text-white focus:ring-yellow-500 focus:bg-yellow-500 focus:text-white`,
+    indigo: `border-indigo-300 text-indigo-700 bg-indigo-100 hover:bg-indigo-900 hover:text-white active:bg-indigo-900 active:text-white focus:ring-indigo-500 focus:bg-indigo-900 focus:text-white`,
 };
 const iconsAttrs = {
     className: 'h-5 w-5 text-current',
